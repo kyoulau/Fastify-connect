2b9800e7438f757e385346ecfb18bd0f19dae10b
kyoulau <laucosan05@gmail.com>
2025-02-19 19:41:11 -0300

    docker configs

--- env.ts
+++ env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3030),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url()
 })
 
 export const env = envSchema.parse(process.env)
